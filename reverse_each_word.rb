f1b67a024031c5517ceea5fc05f1d817eae63efa
Jeff Swisher <jefftswisher@gmail.com>
2019-10-10 16:51:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,11 +7,3 @@ def reverse_each_word_with_each(string)
   new_array.join(" ")
 end 
 
-def reverse_each_word(string)
-  array = string.split(" ")
-  new_array = []
-  array.collect do |string|
-    new_array << string.reverse(" ")
-  end 
-  new_array.join(" ")
-end 
