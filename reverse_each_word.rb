d8a7b05fc9551c012e774d9e80729acff51f4b71
Jeff Swisher <jefftswisher@gmail.com>
2019-10-10 16:52:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
-def reverse_each_word_with_each(string)
-    current_array = string.split(" ")
+def reverse_each_word_with_each(sentence1)
+    current_array = sentence1.split(" ")
     new_array = []
-    current_array.each do |string|
-    new_array << string.reverse
+    current_array.each do |sentence1|
+    new_array << sentence1.reverse
   end
   new_array.join(" ")
 end 
